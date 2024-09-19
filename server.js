babce3dfcd25125c88d9018972840fb7cfc8d998
Samuel-DeSantis <samuel.i.desantis@gmail.com>
2024-09-18 22:49:28 -0600

    re:Added layout and stylesheet

--- server.js
+++ server.js
@@ -1,21 +1,39 @@
 const express = require('express')
-const app = express()
+const expressLayouts = require('express-ejs-layouts')
 
+const app = express()
+const PORT = 3000
+
+app.use(expressLayouts)
+app.use(express.static('public'))
+app.set('layout', './layout')
 app.set('view engine', 'ejs')
 
+//=================
+// General Routes
+//=================
 app.get('/', (req, res) => {
-  res.render('_general/home')
+  res.render('_general/home', {title: 'Home'})
 })
 
 app.get('/about', (req, res) => {
-  res.render('_general/about')
+  res.render('_general/about', {title: 'About'})
 })
 
 app.get('/contact', (req, res) => {
-  res.render('_general/contact')
+  res.render('_general/contact', {title: 'Contact'})
 })
 
+//=================
+// Protected Routes
+//=================
+
+
+
+//=================
+// Routers
+//=================
 const userRouter = require('./routes/users')
 app.use('/users', userRouter)
 
-app.listen(3000)
+app.listen(PORT)

0735c770c4f9f81157e1eb2207483dc6a51c2718
Samuel-DeSantis <samuel.i.desantis@gmail.com>
2024-09-23 23:41:10 -0600

    reading and writing to mongodb

--- routes/users.js
+++ routes/users.js
@@ -1,31 +1,57 @@
 const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcrypt')
-
+const User = require('../models/user')
 
 router.get('/signup', (req, res) => {
   res.render('users/signup')
 })
 
-router.post('/', async (req, res) => {
-  const uname = req.body.username
+router.post('/signup', async (req, res) => {
+  const username = req.body.username
   try {
     const saltRounds = 10; // Adjust salt rounds as needed
     const salt = await bcrypt.genSalt(saltRounds);
     console.log(salt)
     const hash = await bcrypt.hash(req.body.password, salt);
     console.log(hash)
+    const user = new User({
+      username: username,
+      password_digest: hash
+    })
+    user.save()
+      .then(() => console.log("Succesfully Added Record"))
+      .catch(err => console.log(err))
   } catch (error) {
     console.error("Error hashing password:", error);
     throw error;
   }
   console.log(req.body.password)
-  res.render('users/show', {username: uname})
+
+  res.render('users/show', {username: username})
 })
 
-// router.get('/signup', (req, res) => {
-//   res.render('users/signup')
-// })
+router.get('/signin', (req, res) => {
+  res.render('users/signin')
+})
+
+router.post('/signin', async (req, res) => {
+  const username = req.body.username
+  console.log(req.body.username)
+  User.find({ username: req.body.username})
+    .then(user => {
+      bcrypt.compare(req.body.password, user[0].password_digest)
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => console.error(err.message))
+    })
+  // const username = req.body.username
+
+  res.render('users/show', {username: username})
+
+  // res.render('users/show', {username: username})
+})
 
 // router.post('/', (req, res) => {
 //   console.log(req.body)
@@ -35,5 +61,28 @@ router.post('/', async (req, res) => {
 // router.get('/users', (req, res) => {
 //   res.render('users/show')
 // })
+
+// router.post('/', async (req, res) => {
+  // const note1 = new Note({
+  //   title: 'Groceries',
+  //   content: 'Apples, pears, beans',
+  //   tags: ['shopping', 'budget']
+  // })
+  // try {
+  //   const note1Save = await note1.save()
+  //   res.status(200).json(note1Save)
+  // } catch (err) {
+  //   res.status(400).json({message: err.message})
+  // }
+// })
+
+// const note1 = new Note({
+//   title: 'Groceries',
+//   content: 'Apples, pears, beans',
+//   tags: ['shopping', 'budget']
+// })
+// note1.save()
+//   .then(() => console.log('SUCCESSFULLY ADDED RECORD'))
+//   .catch((err) => console.log(`ERROR::${err}`))
 
 module.exports = router
--- server.js
+++ server.js
@@ -6,9 +6,33 @@ require('dotenv').config()
 const app = express()
 const PORT = 3000
 
-// mongoose.connect(process.env.MONGODB_URL)
-//   .then(() => {console.log(`CONNECTION::SUCCESSFUL`)})
-//   .catch((err) => {console.log(`CONNECTION::ERROR::${err}`)})
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(process.env.MONGODB_URL, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {console.log(`CONNECTION::SUCCESSFUL`)})
+  .catch((err) => {console.log(`CONNECTION::ERROR::${err}`)})
 
 app.use(expressLayouts)
 app.use(express.static('public'))
